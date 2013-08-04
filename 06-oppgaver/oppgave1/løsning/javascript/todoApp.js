d32f5e9e1998caab1a1b59b103a2dfafe0a6139f
Helge Fredheim <helge.fredheim@bekk.no>
2013-08-04 21:43:14 +0200

    Endret HTML-presentasjon til ny versjon av remark

--- 06-oppgaver/oppgave1/løsning/javascript/todoApp.js
+++ 06-oppgaver/oppgave1/løsning/javascript/todoApp.js
@@ -1,59 +1,95 @@
-var itemList = [];
-var itemListElement = $("#itemList");
-var formElement = $("#addItemForm");
+var todoListElement = $("#todoList");
+var formElement = $("#addTodoForm");
 
-var renderItemList = function() {
+// Rendering
+
+var renderTodoList = function() {
 	var html = "";
-	for(item in itemList) {
-		html = html + "<li>" + itemList[item].task + " <a href='#' class='btn remove'>Remove</a><a href='#' class='btn change'>Change</a></li>";
+	for(todo in todoList) {
+		// Løper igjennom listen og konkatinerer html
+		html = html + "<li>" + todoList[todo].render(); + "</li>";
 	}
-	itemListElement.html(html);
+	todoListElement.html(html);
 };
 
-var Item = function(task) {
+// Todo
+
+var Todo = function(task) {
 	this.task = task;
 };
 
-var addItem = function(task) {
-	var i = new Item(task);
-	itemList.push(i);
-	renderItemList();
+Todo.prototype.render = function() {
+	return this.task + " <a href='#' class='btn remove'>Remove</a> <a href='#' class='btn change'>Change</a></li>";
 };
 
-var removeItem = function(index) {
-	itemList.splice(index, 1);
-	renderItemList();
+// Todo List
+
+var todoList = [];
+
+// var todoList = (function() {
+// 	var savedList = JSON.parse(localStorage.getItem("todos"));
+// 	var returnedList = [];
+// 	if(savedList) {
+// 		for(i = 0; i < savedList.length; i++) {
+// 			var t = new Todo(savedList[i].task);
+// 			returnedList.push(t);
+// 		}
+// 	}
+// 	return returnedList;
+// })(); 
+
+// var saveTodoList = function() {
+// 	localStorage.setItem("todos", JSON.stringify(todoList))
+// };
+
+// Add/remove item
+
+var addTodo = function(task) {
+	var todo = new Todo(task); // Lager en ny Todo
+	todoList.push(todo); // Legger den til i arrayet
+	$(window).trigger("todolistChanged"); 
 };
 
-var addItemHandler = function(e) {
-	e.preventDefault(); 
-	var input = $(this).find("input");
+var removeTodo = function(index) {
+	todoList.splice(index, 1);
+	$(window).trigger("todolistChanged");
+};
+
+// Event handlers
+
+var addTodoHandler = function(e) {
+	e.preventDefault(); // Forhindrer default oppførsel 
+	var input = $(this).find("input"); 
 	var task = input.val();
-	if($.trim(task) !== "") {
-		addItem(task);
-		input.val("");
+	if($.trim(task) !== "") { // Sjekker om det er skrevet inn en task
+		addTodo(task); // Lager i så fall en ny todo
+
+		input.val(""); // Gjør klart for å skrive inn ny todo
+		input.focus(); 
 	} 
 };
 
-var removeItemHandler = function(e) {
+var removeTodoHandler = function(e) {
 	var index = $(this).closest("li").index(); 
-	removeItem(index);
+	removeTodo(index);
 };
 
-var changeItemHandler = function(e) {
+var changeTodoHandler = function(e) {
 	var index = $(this).closest("li").index(); 
-	var task = itemList[index].task;
+	var task = todoList[index].task;
 	formElement.find("input").val(task);
-	disableFormEvents();
+	disableSubmitEvents();
 	enableChange(index); 
 };
 
-var disableFormEvents = function() {
+// Event listening
+
+var disableSubmitEvents = function() {
 	formElement.off("submit");
 };
 
 var enableAdding = function() {
-	formElement.on("submit", addItemHandler);
+	formElement.on("submit", addTodoHandler);
 };
 
 var enableChange = function(index) {
@@ -61,17 +97,20 @@ var enableChange = function(index) {
 	input.focus(); 
 	formElement.on("submit", function(event) {
 		event.preventDefault(); 
-		var newTask = input.val(); 
-		if($.trim(newTask) !== "") {
-			itemList[index].task = newTask; 
-			renderItemList(); 
-			disableFormEvents(); 
+		var modifiedTask = input.val(); 
+		if($.trim(modifiedTask) !== "") {
+			todoList[index].task = modifiedTask; 
+			$(window).trigger("todolistChanged"); 
+			disableSubmitEvents(); 
 			enableAdding(); 
 			input.val("");
 		}
 	});
-}
+};
 
 enableAdding(); 
-itemListElement.on("click", ".remove", removeItemHandler);
-itemListElement.on("click", ".change", changeItemHandler);
+todoListElement.on("click", ".remove", removeTodoHandler);
+todoListElement.on("click", ".change", changeTodoHandler);
+$(window).on("todolistChanged", renderTodoList);
+// $(window).on("todolistChanged", saveTodoList);
+// renderTodoList(); 
