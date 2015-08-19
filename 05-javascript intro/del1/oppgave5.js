ffdec98edb5c451b7df1e9941e38c3365251e27e
ingvild <ingvildindr@gmail.com>
2015-08-19 12:48:50 +0200

    Fjernet løsning på oppgaven

--- 05-javascript intro/del1/oppgave5.js
+++ 05-javascript intro/del1/oppgave5.js
@@ -1,7 +1,7 @@
 // Make the doubleMe function return the value doubled for numbers and the string repeated twice for strings.
 
 function doubleMe (val) {
-    return val+val;
+
 }
 
 test("doubleMe", function() {
